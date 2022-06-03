da0117e3f9b461e58ab2bd66dff72f0ca6486a96
DhavalContrivertech <dhaval@contrivertech.com>
2022-06-03 10:52:24 +0530

    Sprint controller

---
+++ controller/SprintController.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose")
+const Sprint = require("../model/Sprint")
+
+exports.RegisterSprint = async (req, res) => {
+    try {
+        
+    } catch (error) {
+        res.staus(200)
+    }
+}
