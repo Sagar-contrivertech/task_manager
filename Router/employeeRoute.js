91d5da8b47e34309a85e3ac2a0566c720c5b619d
Sagar-contrivertech <97974995+Sagar-contrivertech@users.noreply.github.com>
2022-06-03 12:35:43 +0530

    chnage in controller

--- Router/employeeRoute.js
+++ Router/employeeRoute.js
@@ -1,17 +1,21 @@
 const express = require("express");
 const router = express.Router();
 const employeeController = require("../controller/employeeController");
-const {isAuthenticated , authorizeRoles} = require("../middleware/Auth");
+const { isAuthenticated, authorizeRoles } = require("../middleware/Auth");
 
 
-router.post("/employeereg",isAuthenticated, employeeController.employeeRegister);
+router.post("/employeereg", isAuthenticated, employeeController.employeeRegister);
 
-router.get("/employeeget",isAuthenticated, employeeController.getEmploy);
+router.get("/employeeget", isAuthenticated, employeeController.getEmploy);
 
-router.get("/employeeget/:id",isAuthenticated, employeeController.getEmployById);
+router.get("/employeeget/:id", isAuthenticated, employeeController.getEmployById);
 
-router.put("/employeeupdate/:id",isAuthenticated, employeeController.updateById);
+router.put("/employeeupdate/:id", isAuthenticated, employeeController.updateById);
 
-router.delete("/employeedelete/:id",isAuthenticated, employeeController.deleteById);
+router.delete("/employeedelete/:id", isAuthenticated, employeeController.deleteById);
+
+//leaves route include in employee
+
+router.post("/leaves", employeeController.addleaves)
 
 module.exports = router;
